807f753589405641ebcfa881cd9615f6f60322af
Davide <davidegorbani@gmail.com>
2025-08-05 16:50:44 +0200

    clean up flight control utils

--- src/flight-controller/utils/src/FlightControlUtils.cpp
+++ src/flight-controller/utils/src/FlightControlUtils.cpp
@@ -1,5 +1,20 @@
 #include "FlightControlUtils.h"
 
+void addVectorOfStringToProperty(yarp::os::Property& prop,
+                                 std::string key,
+                                 const std::vector<std::string>& list)
+{
+    prop.addGroup(key);
+
+    yarp::os::Bottle& bot = prop.findGroup(key).addList();
+
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        bot.addString(list[i].c_str());
+    }
+    return;
+}
+
 yarp::os::Property paramsAsProperty(yarp::robotinterface::ParamList& params)
 {
     yarp::robotinterface::ParamList p = yarp::robotinterface::mergeDuplicateGroups(params);
@@ -59,59 +74,6 @@ bool readXMLFile(
     return true;
 }
 
-void addVectorOfStringToProperty(yarp::os::Property& prop,
-                                 std::string key,
-                                 const std::vector<std::string>& list)
-{
-    prop.addGroup(key);
-
-    yarp::os::Bottle& bot = prop.findGroup(key).addList();
-
-    for (size_t i = 0; i < list.size(); i++)
-    {
-        bot.addString(list[i].c_str());
-    }
-    return;
-}
-
-void convertDegToRad(const yarp::sig::Vector& vecDeg, yarp::sig::Vector& vecRad)
-{
-    if (vecDeg.size() != vecRad.size())
-    {
-        yError() << "convertDegToRad: wrong vector size";
-        return;
-    }
-
-    for (size_t i = 0; i < vecDeg.size(); i++)
-    {
-        vecRad[i] = (M_PI / 180.0) * vecDeg[i];
-    }
-}
-
-void convertRadToDeg(const yarp::sig::Vector& vecRad, yarp::sig::Vector& vecDeg)
-{
-    if (vecDeg.size() != vecRad.size())
-    {
-        yError() << "convertRadToDeg: wrong vector size";
-        return;
-    }
-
-    for (size_t i = 0; i < vecRad.size(); i++)
-    {
-        vecDeg[i] = (180.0 / M_PI) * vecRad[i];
-    }
-}
-
-iDynTree::Matrix3x3 skewSymmetricFrom3DMatrix(const iDynTree::Matrix3x3& inputMatrix)
-{
-    // Given a 3x3 matrix, extracts its skew-symmetric part
-    iDynTree::Matrix3x3 outputMatrix;
-
-    iDynTree::toEigen(outputMatrix)
-        = 0.5 * (iDynTree::toEigen(inputMatrix) - iDynTree::toEigen(inputMatrix).transpose());
-    return outputMatrix;
-}
-
 Eigen::Matrix3d fromVecToSkew(const Eigen::Vector3d& inputVect)
 {
     Eigen::Matrix3d skew(3, 3);
@@ -152,72 +114,4 @@ bool getParameterAndCheckSize(
         return false;
     }
     return true;
-}
-
-Eigen::Vector3d computeRotationErrorInBodyCoord(const Eigen::Ref<Eigen::Matrix3d> R_des,
-                                                const Eigen::Ref<Eigen::Matrix3d> R_curr)
-{
-    // Given two rotation matrices, computes the error between them. Valid when the equations
-    // that use this error as feedback are expressed in body coordinates.
-    Eigen::Vector3d error;
-
-    error = 0.5 * fromSkewToVec(R_des.transpose() * R_curr - R_curr.transpose() * R_des);
-
-    return error;
-}
-
-Eigen::Vector3d computeRotationErrorInInertialCoord(const Eigen::Ref<Eigen::Matrix3d> R_des,
-                                                    const Eigen::Ref<Eigen::Matrix3d> R_curr)
-{
-    // Given two rotation matrices, computes the error between them
-    Eigen::Vector3d error;
-
-    error = 0.5 * fromSkewToVec(R_curr * R_des.transpose() - R_des * R_curr.transpose());
-
-    return error;
-}
-
-// Helper function to calculate the filter coefficient (alpha)
-void LowPassFilter::calculateAlpha()
-{
-    double dt = 1.0 / samplingFreq;
-    double RC = 1.0 / (2 * M_PI * cutoffFreq);
-    alpha = dt / (RC + dt);
-}
-
-// Constructor to initialize the filter with a cutoff frequency and sampling frequency
-LowPassFilter::LowPassFilter(double cutoffFreq, double samplingFreq)
-    : cutoffFreq(cutoffFreq)
-    , samplingFreq(samplingFreq)
-    , prevY(0.0)
-{
-    calculateAlpha();
-}
-
-// Set a new cutoff frequency and recalculate alpha
-void LowPassFilter::setCutoffFrequency(double newCutoffFreq)
-{
-    cutoffFreq = newCutoffFreq;
-    calculateAlpha();
-}
-
-// Set a new sampling frequency and recalculate alpha
-void LowPassFilter::setSamplingFrequency(double newSamplingFreq)
-{
-    samplingFreq = newSamplingFreq;
-    calculateAlpha();
-}
-
-// Process the input signal and return the filtered output
-double LowPassFilter::process(double input)
-{
-    double output = alpha * input + (1 - alpha) * prevY;
-    prevY = output; // Store the output for the next step
-    return output;
-}
-
-// Reset the filter's state
-void LowPassFilter::reset()
-{
-    prevY = 0.0;
 }
